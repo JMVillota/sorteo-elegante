fb28ed426c69bd56adac58fd8db19428c28ebd11
JMVillota <jeffersonvillota@gmail.com>
2025-06-13 01:51:09 -0500

    CA

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Para rutas relativas
   server: {
-    host: '192.168.1.20', // Escucha en todas las interfaces de red (o usa '192.168.2.154')
-    port: 5173,      // Puerto opcional (puedes cambiarlo si lo necesitas)
+    host: '192.168.1.20',
+    port: 5173,
   },
 });
